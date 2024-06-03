7ef4296365eeb2e820968171af7159205ff5fd19
Loredo Livea (CaP/ETS2) <loi7ca@bosch.com>
2024-06-03 17:07:40 -0300

    InitializeFila

---
+++ src/pilha/filas/Fila.java
@@ -0,0 +1,16 @@
+package pilha.filas;
+
+import pilha.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+
+    public Fila(){
+        super();
+    }
+
+    public Fila(int capacidade){
+        super(capacidade);
+    }
+
+
+}
---
+++ src/pilha/filas/aulas/Aula19.java
@@ -0,0 +1,12 @@
+package pilha.filas.aulas;
+
+import pilha.filas.Fila;
+
+public class Aula19 {
+    public static void main(String[] args) {
+        Fila<Integer> fila = new Fila<>();
+
+        System.out.println(fila.estaVazia());
+        System.out.println(fila.tamanho());
+    }
+}
