85176ec12e332bee9421c551eb47dce4b60561a0
liveasoaresloredo <liveasoaresloredo@outlook.com>
2024-06-07 10:04:33 -0300

    FinishListaEncadeada

---
+++ src/listaEncadeada/aulas/CompararNumero.java
@@ -0,0 +1,11 @@
+package listaEncadeada.aulas;
+
+import java.util.Comparator;
+
+public class CompararNumero implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2){
+        if (o1.intValue() == o2.intValue()) return 0;
+        return o1.intValue() < o2.intValue() ? -1 : 1;
+    }
+}
--- src/listaEncadeada/aulas/ListaEncadeadaTeste.java
+++ src/listaEncadeada/aulas/ListaEncadeadaTeste.java
@@ -2,17 +2,89 @@ package listaEncadeada.aulas;
 
 import listaEncadeada.ListaEncadeada;
 
+import java.util.LinkedList;
+
 public class ListaEncadeadaTeste {
 
     public static void main(String[] args) {
 //        testesIniciais();
 //        adicionaInicio();
 //        adicionaPosicao();
-        removeInicio();
+//        removeInicio();
+        removeFinal();
+//        removePosicao();
+//        insereOrdenado();
+//        testeLinkedList();
+    }
+
+    public static void testeLinkedList(){
+        LinkedList<Integer> lista = new LinkedList<>();
+
+        lista.add(1);
+        lista.add(0);
+        lista.add(3);
+
+        System.out.println(lista.contains(1));
+
+        lista.add(2, 2);
+
+        System.out.println(lista);
+
+        lista.remove();
+        System.out.println(lista);
+
+        lista.remove(0);
+        lista.removeFirst();
+        lista.removeLast();
+        System.out.println(lista);
+    }
+
+    public static void insereOrdenado(){
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        CompararNumero comparator = new CompararNumero();
+        lista.adicionaOrdenado(5, comparator);
+        System.out.println(lista);
+        lista.adicionaOrdenado(1, comparator);
+        System.out.println(lista);
+        lista.adicionaOrdenado(4, comparator);
+        System.out.println(lista);
+        lista.adicionaOrdenado(2, comparator);
+        System.out.println(lista);
+        lista.adicionaOrdenado(3, comparator);
+        System.out.println(lista);
+        lista.adicionaOrdenado(6, comparator);
+        System.out.println(lista);
+        lista.adicionaOrdenado(5, comparator);
+        System.out.println(lista);
+    }
+
+    public static void removePosicao(){
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        lista.adiciona(1);
+        lista.adiciona(2);
+        lista.adiciona(3);
+        lista.adiciona(4);
+        lista.adiciona(5);
+
+        System.out.println("Elemento removido: " + lista.remove(4));
+        System.out.println("Lista: " + lista);
     }
 
     public static void removeFinal(){
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
 
+        lista.adiciona(1);
+        lista.adiciona(2);
+        lista.adiciona(3);
+
+        System.out.println("Elemento removido: " + lista.removeFinal());
+        System.out.println("Lista: " + lista);
+
+        System.out.println("Elemento removido: " + lista.removeFinal());
+        System.out.println("Lista: " + lista);
+
+        System.out.println("Elemento removido: " + lista.removeFinal());
+        System.out.println("Lista: " + lista);
     }
 
     public static void removeInicio(){
@@ -30,35 +102,25 @@ public class ListaEncadeadaTeste {
 
         System.out.println("Elemento removido: " + lista.removeInicio());
         System.out.println("Lista: " + lista);
-
     }
-
 
     public static void adicionaPosicao(){
         ListaEncadeada<Integer> lista = new ListaEncadeada<>();
-//        lista.adiciona(-1, 1);
+
+//        lista.adiciona(-1,1);
         lista.adiciona(1);
         lista.adiciona(2);
         lista.adiciona(4);
 
-        lista.adiciona(0, 0);
-        lista.adiciona(4, 5);
-        lista.adiciona(2, 3);
+//        lista.adiciona(5,0);
+        lista.adiciona(0,0);
+        lista.adiciona(4,5);
+        lista.adiciona(2,3);
 
         System.out.println(lista);
-
-//        lista.adiciona(5, 0);
     }
+
     public static void adicionaInicio(){
-        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
-        lista.adicionaInicio(3);
-        lista.adicionaInicio(2);
-        lista.adicionaInicio(1);
-
-        System.out.println(lista);
-    }
-
-    public static void testesIniciais(){
         ListaEncadeada<Integer> lista = new ListaEncadeada<>();
         lista.adiciona(1);
 
@@ -71,17 +133,20 @@ public class ListaEncadeadaTeste {
         lista.adiciona(3);
         System.out.println(lista);
 
-        System.out.println("******** busca por elemento ");
-        System.out.println(lista.busca(0));
+//        lista.limpa();
+//        System.out.println(lista);
+
+        System.out.println("******* busca por elemento");
         System.out.println(lista.busca(1));
         System.out.println(lista.busca(2));
         System.out.println(lista.busca(3));
+        System.out.println(lista.busca(0));
 
-        System.out.println("******** busca por posição ");
+        System.out.println("******* busca por posição");
         System.out.println(lista.buscaPorPosicao(0));
         System.out.println(lista.buscaPorPosicao(1));
         System.out.println(lista.buscaPorPosicao(2));
-//        System.out.println(lista.buscaPorPosicao(-1));
+//        System.out.println(lista.busca(-1));
 //        System.out.println(lista.buscaPorPosicao(4));
     }
 }
